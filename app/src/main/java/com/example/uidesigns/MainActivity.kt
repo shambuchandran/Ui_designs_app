a46f8e46d4a9234e7e29a7407e95568cda9b44b6
shambu <shambuchandrannair@gmail.com>
2024-03-24 00:36:46 +0530

    first commit

---
+++ app/src/main/java/com/example/uidesigns/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.uidesigns
+
+import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toolbar
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/uidesigns/MainActivitypayments.kt
@@ -0,0 +1,26 @@
+package com.example.uidesigns
+
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivitypayments : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main_activitypayments)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val progressBar: View = findViewById(R.id.progressBar)
+        val progress = 30
+        val progressBarWidth = (progress / 100.0 * resources.displayMetrics.widthPixels).toInt()
+        progressBar.layoutParams.width = progressBarWidth
+        progressBar.requestLayout()
+    }
+}
