6bf769af3f51e66940da206452c83bad7106094c
shambu <shambuchandrannair@gmail.com>
2024-03-24 20:27:29 +0530

    second commit

--- app/src/main/java/com/example/uidesigns/MainActivitymanagestore.kt
+++ app/src/main/java/com/example/uidesigns/MainActivitymanagestore.kt
@@ -1,20 +1,55 @@
 package com.example.uidesigns
 
+import android.content.Intent
+import android.media.RouteListingPreference.Item
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.uidesigns.databinding.ActivityMainActivitymanagestoreBinding
 
 class MainActivitymanagestore : AppCompatActivity() {
+    private lateinit var binding: ActivityMainActivitymanagestoreBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main_activitymanagestore)
+        binding = ActivityMainActivitymanagestoreBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        binding.bottomNavigationView.setOnItemReselectedListener { MenuItem ->
+            when (MenuItem.itemId) {
+                R.id.home -> {
+                    startActivity(Intent(this, MainActivity::class.java))
+                    true
+                }
+
+                R.id.account -> {
+                    startActivity(Intent(this, MainActivitypayments::class.java))
+                    true
+                }
+                R.id.products -> {
+                    startActivity(Intent(this, MainActivitypremium::class.java))
+                    true
+                }
+                R.id.orders -> {
+                    startActivity(Intent(this, MainActivityorder::class.java))
+                    true
+                }
+
+
+                else -> false
+
+            }
+        }
     }
 }
+
+
--- app/src/main/java/com/example/uidesigns/MainActivityorder.kt
+++ app/src/main/java/com/example/uidesigns/MainActivityorder.kt
@@ -1,32 +1,20 @@
 package com.example.uidesigns
 
 import android.os.Bundle
-import android.view.View
-import android.widget.ListView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivitypayments : AppCompatActivity() {
+class MainActivityorder : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main_activitypayments)
+        setContentView(R.layout.activity_main_activityorder)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val progressBar: View = findViewById(R.id.progressBar)
-        val progress = 30
-        val progressBarWidth = (progress / 100.0 * resources.displayMetrics.widthPixels).toInt()
-        progressBar.layoutParams.width = progressBarWidth
-        progressBar.requestLayout()
-
-        val listView = findViewById<ListView>(R.id.listviewpayments)
-        listView.smoothScrollToPosition(0, listView.height)
-
     }
-
 }
